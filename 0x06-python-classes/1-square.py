1701210faf9c04e771e26b6d2bf0c8d834d6ff9d
onyxvail <ons.hfi@gmail.com>
2023-06-27 12:17:01 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Definig a Square class """
+
+
+class Square:
+    """ Square """
+    def __init__(self, size):
+        """ Initializing a square
+        args:
+            size: size of square"""
+        self.__size = size
