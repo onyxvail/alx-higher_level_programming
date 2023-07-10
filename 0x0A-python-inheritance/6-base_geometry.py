edccb95b3fb5ebc555d8d25ed8a5232d6e8014ad
onyxvail <ons.hfi@gmail.com>
2023-07-10 15:04:58 +0100

    fixing task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Base Geometry module"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
+    """Represent base geometry."""
 
     def area(self):
-        """Public instance method"""
+        """Not implemented."""
         raise Exception("area() is not implemented")
