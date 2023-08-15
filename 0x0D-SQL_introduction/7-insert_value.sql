f835ec06e7c864c729d208a496e665a1e93be296
onyxvail <ons.hfi@gmail.com>
2023-08-15 23:14:17 +0100

    fixing error

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
 -- Task 0x07: Inserts a new row in table in MySQL server
 -- Insert into a hbtn_0c_0.first_table
-INSERT INTO `first_table`(`id`, `name`)
-VALUES (89, "Holberton School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
