84fbb6683334d9ba5029c62bd03d59994f95d3e9
onyxvail <ons.hfi@gmail.com>
2023-08-15 23:16:00 +0100

    fixing error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,4 @@
 -- Task 0x11: converts into utf8 database in MySQL server
--- Alter the charset coding in a the database hbtn_0c_0
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Alter the charset coding in a the table hbtn_0c_0.first_table
-ALTER TABLE `hbtn_0c_0`.`first_table` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Alter the charset coding in a the table hbtn_0c_0.first_table.name
-ALTER TABLE `hbtn_0c_0`.`first_table`
-MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
