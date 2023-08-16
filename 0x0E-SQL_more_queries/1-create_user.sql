95a1454ceaf9abf0dd09f755d80345d45752f44a
onyxvail <ons.hfi@gmail.com>
2023-08-17 00:13:12 +0100

    fixing error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,5 @@
 -- Task 0x01: Grant previlges in MySQL server
 
--- Creates a user user_0d_1@localhost
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-            IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all previleges to user_0d_1@localhost
-GRANT ALL PRIVILEGES
-                  ON *.*
-                  TO 'user_0d_1'@'localhost';
+-- creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
