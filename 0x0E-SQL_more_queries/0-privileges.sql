992ece41ed35767cfb6e4da474bc3b884b19e8c6
onyxvail <ons.hfi@gmail.com>
2023-08-17 00:14:28 +0100

    fixing error

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Task 0x00: Grant previlges in MySQL server
--- Lists all previleges of the user user_0d_1
-SHOW GRANTS FOR user_0d_1@localhost;
--- Lists all previleges of the user user_0d_2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
