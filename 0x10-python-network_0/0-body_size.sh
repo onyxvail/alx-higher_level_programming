dc402a3f050daf43afcc14751d5bc81b1b549b31
“onyxvail” <ons.hfi@gmail.com>
2023-09-28 14:17:28 +0100

    task 0 fixing code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Get the URL request that is sent and
 # displays the size of the body of the response.
-curl -s "$1" | wc -c
+curl -sI "$1" | awk -v FS=": " '/^Content-Length/{print $2}'
