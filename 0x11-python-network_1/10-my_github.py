9ae0fc8092a7f0f545390d9493c6a513d553c89a
onyxvail <ons.hfi@gmail.com>
2023-09-29 15:59:31 +0100

    task 9 fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,6 @@ import sys
 
 
 if __name__ == '__main__':
-    r = requests.get('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',
+    r = requests.get('https://api.github.com/user',
                      auth=(sys.argv[1], sys.argv[2]))
     print(r.json().get('id'))
