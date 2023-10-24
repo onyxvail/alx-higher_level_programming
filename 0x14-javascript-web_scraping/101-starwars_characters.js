6e75bde0a93babca07aedccc4eec652894235b0f
onyxvail <ons.hfi@gmail.com>
2023-10-24 13:16:14 +0100

    ADVANCED

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
-const axios = require('axios');
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
-const movieId = process.argv[2];
-
-axios.get(apiUrl + movieId)
-  .then((response) => {
-    const characters = response.data.characters;
-    console.log(characters);
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
   });
+}
